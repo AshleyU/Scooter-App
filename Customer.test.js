d204c07f1754483e7922713e7e96f43a49a65a8e
Ashley U <ashley.umetsu@gmail.com>
2021-07-01 17:25:41 -0500

    Adding testing

--- Customer.test.js
+++ Customer.test.js
@@ -1,6 +1,7 @@
 const Customer = require('./Customer');
 
 const customer1 = new Customer("Alyssa", 21);
+const customer2 = new Customer("Sebastian", 11);
 
 test('downloaded app', () => {
     expect(customer1.downloadApp()).toBe(true);
@@ -8,5 +9,8 @@ test('downloaded app', () => {
 
 test('is registered', () => {
     expect(customer1.registeredDetails()).toBe("Customer is registered.");
+    expect(customer2.registeredDetails()).toBe("You must be 18 or older to register.");
 });
+
+
 
--- scooterKiosk.test.js
+++ scooterKiosk.test.js
@@ -1,9 +1,11 @@
 const ScooterKiosk = require('./ScooterKiosk');
 
 const scooterK1 = new ScooterKiosk(10, 5);
+const scooterK2 = new ScooterKiosk(15, 0);
 
 test('Can rent scooter', () => {
     expect(scooterK1.rentScooter()).toBe("Scooter rented. Enjoy!");
+    expect(scooterK2.rentScooter()).toBe("This scooter is not charged.");
 });
 
 test('Can return scooter', () => {
